e3484257cc5cf2f25df66c1510b101a83a454109
svifty7 <svifty7@gmail.com>
2022-03-22 01:00:35 +0300

    fixed gh-pages publication

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     '*.{js,jsx,vue,ts,tsx}': [
         'vue-cli-service lint',
-        'yarn run build',
-        'gh-pages -d dist -b gh-pages'
+        'yarn run deploy'
     ]
 };
