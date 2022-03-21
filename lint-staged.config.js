7dd96a31482229025a5e29074219d87582a8fafe
svifty7 <svifty7@gmail.com>
2022-03-22 00:45:12 +0300

    added gh-pages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    '*.{js,jsx,vue,ts,tsx}': 'vue-cli-service lint',
+    '*.{js,jsx,vue,ts,tsx}': [
+        'vue-cli-service lint',
+        'vue-cli-service build',
+        'gh-pages -d dist -b gh-pages'
+    ]
 };
